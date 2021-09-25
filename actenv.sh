2ff94d06fd441a59a6e775caf5ed8fc1110e7bbb
ExtremeViscent <limitviscent@gmail.com>
2021-09-25 15:21:02 +0000

    edited

--- actenv.sh
+++ actenv.sh
@@ -2,6 +2,10 @@
 
 export PATH=/opt/anaconda3/bin:$PATH
 export LD_LIBRARY_PATH=/opt/anaconda3/lib:$LD_LIBRARY_PATH
+export PATH=/opt/ucc/bin:$PATH
+export LD_LIBRARY_PATH=/opt/ucc/lib:$LD_LIBRARY_PATH
+export PATH=/opt/ucx/bin:$PATH
+export LD_LIBRARY_PATH=/opt/ucx/lib:$LD_LIBRARY_PATH
 . /opt/anaconda3/etc/profile.d/conda.sh
 module load ucc-0.1
 module load ucx-1.11
