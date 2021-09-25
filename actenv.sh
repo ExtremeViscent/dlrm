0653b3b7dd6e699fc0346b1097c7e248ad349d78
ExtremeViscent <limitviscent@gmail.com>
2021-09-25 14:16:54 +0000

    torch_ucc included

---
+++ actenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/opt/anaconda3/bin:$PATH
+export LD_LIBRARY_PATH=/opt/anaconda3/lib:$LD_LIBRARY_PATH
+. /opt/anaconda3/etc/profile.d/conda.sh
+module load ucc-0.1
+module load ucx-1.11
+module load nvhpc
+conda activate dlrm
